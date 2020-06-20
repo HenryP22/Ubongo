1eaad120812be032dbfd7fd76e55a9fd401750e6
HenryP22 <50087072+HenryP22@users.noreply.github.com>
2020-06-20 18:57:10 -0500

    Mejor distribución de modulos
    se pusieron en uso los modulos vacios para no saturar el modulo Tablero.py

--- Code/gemas.py
+++ Code/gemas.py
@@ -1,95 +1,95 @@
-import pygame, sys
-
-# Setup pygame/window ---------------------------------------- #
-mainClock = pygame.time.Clock()
-from pygame.locals import *
-
-pygame.init()
-pygame.display.set_caption('Ubongo')
-screen = pygame.display.set_mode((1280, 720), 0, 32)
-
-font = pygame.font.SysFont(None, 20)
-
-tableroImg = pygame.image.load('tablero ubongo.png')
-def tabla():
-    screen.blit(tableroImg,(0,0))
-
-verdeImg = pygame.image.load('gema verde.png')
-rojaImg = pygame.image.load('gema roja.png')
-moradaImg = pygame.image.load('gema morada.png')
-marronImg = pygame.image.load('gema marron.png')
-azulImg = pygame.image.load('gema azul.png')
+import pygame
 amarillaImg = pygame.image.load('gema amarilla.png')
+azulImg = pygame.image.load('gema azul.png')
+marronImg = pygame.image.load('gema marron.png')
+moradaImg = pygame.image.load('gema morada.png')
+rojaImg = pygame.image.load('gema roja.png')
+verdeImg = pygame.image.load('gema verde.png')
 
-def verde():
-    x = 425
-    y=100
-    for i in range (6):
-        for j in range(12):
-            screen.blit(verdeImg,(x,y))
-            if(j<3):
-                x = x + 62
-            if (j>=3):
-                x= x+ 63
-        x = 425
-        y = y + 51
+def GemasJ1(j1, screen):
+    x = 120
+    y= 434
+    n = len(j1.ruta)
+    for i in range(n):
+        if j1.colores[i] == 1:
+            screen.blit(amarillaImg, (x, y))
+        elif j1.colores[i] == 2:
+            screen.blit(azulImg, (x, y))
+        elif j1.colores[i] == 3:
+            screen.blit(marronImg, (x, y))
+        elif j1.colores[i] == 4:
+            screen.blit(moradaImg, (x, y))
+        elif j1.colores[i] == 5:
+            screen.blit(rojaImg, (x, y))
+        elif j1.colores[i] == 6:
+            screen.blit(verdeImg, (x, y))
+        x = x + 35
+        if(x>360):
+            x=120
+            y=y+32
 
-def roja():
-    screen.blit(rojaImg,(0,0))
+def GemasJ2(j2,screen):
+    x = 380
+    y= 434
+    n = len(j2.ruta)
+    for i in range(n):
+        if j2.colores[i] == 1:
+            screen.blit(amarillaImg, (x, y))
+        elif j2.colores[i] == 2:
+            screen.blit(azulImg, (x, y))
+        elif j2.colores[i] == 3:
+            screen.blit(marronImg, (x, y))
+        elif j2.colores[i] == 4:
+            screen.blit(moradaImg, (x, y))
+        elif j2.colores[i] == 5:
+            screen.blit(rojaImg, (x, y))
+        elif j2.colores[i] == 6:
+            screen.blit(verdeImg, (x, y))
+        x = x + 35
+        if(x>620):
+            x=380
+            y=y+32
 
-def morada():
-    screen.blit(moradaImg,(0,0))
+def GemasJ3(j3,screen):
+    x = 650
+    y= 434
+    n = len(j3.ruta)
+    for i in range(n):
+        if j3.colores[i] == 1:
+            screen.blit(amarillaImg, (x, y))
+        elif j3.colores[i] == 2:
+            screen.blit(azulImg, (x, y))
+        elif j3.colores[i] == 3:
+            screen.blit(marronImg, (x, y))
+        elif j3.colores[i] == 4:
+            screen.blit(moradaImg, (x, y))
+        elif j3.colores[i] == 5:
+            screen.blit(rojaImg, (x, y))
+        elif j3.colores[i] == 6:
+            screen.blit(verdeImg, (x, y))
+        x = x + 35
+        if(x>890):
+            x=650
+            y=y+32
 
-def marron():
-    screen.blit(marronImg,(0,0))
-
-def azul():
-    screen.blit(azulImg,(0,0))
-
-def amarrilla():
-    screen.blit(amarillaImg,(0,0))
-
-tableroImg = pygame.image.load('tablero ubongo.png')
-def tabla():
-    screen.blit(tableroImg,(0,0))
-
-verde()
-
-click = False
-
-
-def main_menu():
-    while True:
-
-
-        screen.fill((0, 0, 0))
-        #draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
-
-        mx, my = pygame.mouse.get_pos()
-
-
-        #pygame.draw.rect(screen, (255, 0, 0), button_1)
-        #pygame.draw.rect(screen, (255, 0, 0), button_2)
-        #pygame.draw.rect(screen, (255, 0, 0), button_3)
-        #pygame.draw.rect(screen, (255, 0, 0), button_4)
-
-        click = False
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-            if event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    click = True
-
-
-        tabla()
-        verde()
-        pygame.display.update()
-        mainClock.tick(60)
-
-main_menu()
+def GemasJ4(j4,screen):
+    x = 920
+    y= 434
+    n = len(j4.ruta)
+    for i in range(n):
+        if j4.colores[i] == 1:
+            screen.blit(amarillaImg, (x, y))
+        elif j4.colores[i] == 2:
+            screen.blit(azulImg, (x, y))
+        elif j4.colores[i] == 3:
+            screen.blit(marronImg, (x, y))
+        elif j4.colores[i] == 4:
+            screen.blit(moradaImg, (x, y))
+        elif j4.colores[i] == 5:
+            screen.blit(rojaImg, (x, y))
+        elif j4.colores[i] == 6:
+            screen.blit(verdeImg, (x, y))
+        x = x + 35
+        if(x>1160):
+            x=920
+            y=y+32
