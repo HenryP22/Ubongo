2caba7fc50a3a1de674fe60b8069d16e15dc1a5f
HenryP22 <50087072+HenryP22@users.noreply.github.com>
2020-06-21 00:32:54 -0500

    correcciones

--- Code/dado.py
+++ Code/dado.py
@@ -1,60 +1,40 @@
-import graphviz
-import random
+import pygame
+imagen1 = pygame.image.load("cara1.png")
+imagen2 = pygame.image.load("cara2.png")
+imagen3 = pygame.image.load("cara3.png")
+imagen4 = pygame.image.load("cara4.png")
+imagen5 = pygame.image.load("cara5.png")
+imagen6 = pygame.image.load("cara6.png")
+def dice(n,screen):
+    if n == 1:
+        screen.blit(imagen1, (620, 580))
+    elif n == 2:
+        screen.blit(imagen2, (620, 580))
+    elif n == 3:
+        screen.blit(imagen3, (620, 580))
+    elif n == 4:
+        screen.blit(imagen4, (620, 580))
+    elif n == 5:
+        screen.blit(imagen5, (620, 580))
+    elif n == 6:
+        screen.blit(imagen6, (620, 580))
 
-
-class TableroUbongo(object):
-    def __init__(self, n):
-        self.G = graphviz.Digraph()
-        self.n = n
-        self.visitados = [False] * n
-        self.colores = [None] * n
-        self.ruta = [None] * n
-
-    def crear(self):
-        gema_list = [1, 2, 3, 4, 5, 6]
-        for i in range(self.n):
-            m = random.choice(gema_list)
-            self.G.node(str(i), str(m))
-            self.colores[i] = m
-
-    def conectarTodo(self):
-        h = 5
-        c = 1
-        for i in range(self.n - 6):
-            for j in range(6):
-                j = (i + h) + c
-                self.G.edge(str(i), str(j))
-                c = c + 1
-                if c == 7:
-                    c = 1
-            h = h - 1
-            if h == -1:
-                h = 5
-
-    def tomarGema(self, a, ultimo):
-        if self.visitados[a] == False:
-            self.G.edge(str(a), str(a + 6))
-            self.G.edge(str(ultimo), str(a))
-            self.visitados[a] = True
-            self.visitados[a + 6] = True
-        else:
-            return
-
-    def mostrar(self):
-        self.G.graph_attr['rankdir'] = 'LR'
-        return graphviz.Source(self.G)
-
-
-class jugador(object):
-    def __init__(self):
-        self.ruta = [None] * 0
-        self.ultimo = 0
-
-    def tomarGema(self, a):
-        self.ruta.append(a)
-        self.ruta.append(a + 6)
-        self.ultimo = a + 6
-
-    def mostrar(self):
-        print(self.ruta)
-        print(self.ultimo)
+def puzzle(enum):
+    if (enum == 1):
+        import Puzzle1
+        Puzzle1.main()
+    if (enum == 2):
+        import Puzzle2
+        Puzzle2.main()
+    if (enum == 3):
+        import Puzzle3
+        Puzzle3.main()
+    if (enum == 4):
+        import Puzzle4
+        Puzzle4.main()
+    if (enum == 5):
+        import Puzzle5
+        Puzzle5.main()
+    if (enum == 6):
+        import Puzzle6
+        Puzzle6.main()
